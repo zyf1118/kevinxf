8565d367d2cf2cb25d5ec242f963ed424f53b04c
zyf1118 <85056532+zyf1118@users.noreply.github.com>
2022-03-04 17:26:36 +0800

    Update xF_DiDi_fruit.py

--- xF_DiDi_fruit.py
+++ xF_DiDi_fruit.py
@@ -8,7 +8,7 @@
 Author: 一风一燕
 功能：滴滴app水果自动任务
 Date: 2021-11-23
-cron: 43 0-3,8,9,12,15,18,22 * * * xF_DiDi_fruit.py
+cron: 43 0-3,8,12,15,18,22 * * * xF_DiDi_fruit.py
 new Env('滴滴app水果自动任务');
 
 
@@ -249,7 +249,7 @@ def get_xpsid():
         response = requests.head (url=url, headers=heards, verify=False)  # 获取响应请求头
         res = response.headers['Location']  # 获取响应请求头
         # print(res)
-        r = re.compile (r'root_xpsid=(.*?)&appid', re.M | re.S | re.I)
+        r = re.compile (r'root_xpsid=(.*?)&channel_id')
         xpsid = r.findall (res)
         xpsid = xpsid[0]
         print (xpsid)
